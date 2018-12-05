368cf51a53a349d94414c0e695c2a7273530a1e2
Thomas Claydon <tomclaydon102@gmail.com>
2018-12-05 17:58:03 -0500

    MVP Works

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,10 +14,10 @@ export const FETCH_CHARACTERS_FAILURE = 'FETCH_CHARACTERS_FAILURE';
 export const fetchCharacters = () => dispatch => {
     dispatch({ type: FETCH_CHARACTERS_START });
     axios
-    .get('https://swapi.co/api/people/')
+    .get('https://cors-anywhere.herokuapp.com/https://swapi.co/api/people/')
     .then(response => {
         console.log(response);
-        dispatch({ type: FETCH_CHARACTERS_SUCCESS, payload: response.results});
+        dispatch({ type: FETCH_CHARACTERS_SUCCESS, payload: response.data.results});
     })
     .catch(err => {
         dispatch({ type: FETCH_CHARACTERS_FAILURE, payload: err});
